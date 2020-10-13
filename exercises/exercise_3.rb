0d53d69bbfa523535208233b8a2899379912a67e
JaeyoungKim <jzero.youngs89@gmail.com>
2020-10-13 06:06:51 +0000

    Exercise 3 and 4

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,13 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+
+@store3 = Store.find_by(id: 3)
+
+puts @store3.name
+
+@store3.destroy
+
+stores = Store.all
+
+puts stores.count
